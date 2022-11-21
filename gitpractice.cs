35139cedab934e1c19cd8b4e9f16efdb9a2d6d0a
jeongik <jeong9700@naver.com>
2022-11-21 21:19:41 +0900

    add from master

--- gitpractice.cs
+++ gitpractice.cs
@@ -1,11 +1,7 @@
 //git 연습
-<<<<<<< HEAD
 //master에서 추가
 //jeongik 브랜치에서 추가
 //두번째 추가
 
 //Resolve in Merge Editor에서 둘 다 추가 후 Accept Both Change
-=======
-//jeongik 브랜치에서 추가
-//두번째 추가
->>>>>>> e28d9cf (gitpractice second add and new file generate from jeongik)
+//master에서 추가

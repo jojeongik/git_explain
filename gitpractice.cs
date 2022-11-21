a81b64cfc36ec0dc9d15f25e5eebd0313ee59b9c
jeongik <jeong9700@naver.com>
2022-11-21 21:01:29 +0900

    complete practice with merging all branch

--- gitpractice.cs
+++ gitpractice.cs
@@ -1,11 +1,7 @@
 //git 연습
-<<<<<<< HEAD
 //master에서 추가
 //jeongik 브랜치에서 추가
 //두번째 추가
 
 //Resolve in Merge Editor에서 둘 다 추가 후 Accept Both Change
-=======
-//jeongik 브랜치에서 추가
-//두번째 추가
->>>>>>> e28d9cf (gitpractice second add and new file generate from jeongik)
+//원격의 두 브랜치와 연결된 로컬의 두 브랜치를 한꺼번에 병합
